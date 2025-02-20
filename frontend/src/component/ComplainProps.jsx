074bf17e6b3e7ee96acdb0cd1e1549fa3784abb5
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-02-20 13:39:24 -0800

    new u

--- frontend/src/component/ComplainProps.jsx
+++ frontend/src/component/ComplainProps.jsx
@@ -5,15 +5,16 @@ const ComplainProps=(props)=> {
     <div>
 
 
-         <div className=' bg-gray-700  w-[250px] h-[220px]  pt-10 text-white
-         rounded-md text-xl mt-20 ml-10'>
+         <div className=' bg-gray-700  w-[250px] h-[240px]  pt-10 text-white
+         rounded-md text-xl mt-20 ml-10 pb-6'>
+          <div></div>
             <h1 className='text-center text-2xl'>{props.name}</h1>
             <p className='text-xl mt-6 text-center'>{props.title}</p>
             <p className='text-xl mt-6 ml-2 '>
             {props.message}</p>
-            <div>
+            <div className='ml-40 mt-3'>
             <i className="fa-solid fa-pen-to-square  text-green-700 text-2xl"></i>
-            <i className="fa-solid fa-trash text-red-700 text-2xl"></i>
+            <i className="fa-solid fa-trash text-red-700 text-2xl ml-4"></i>
             </div>
 
 
