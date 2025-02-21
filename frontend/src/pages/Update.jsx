8e4a34af331a873587dfd076dd4e28e7a342580e
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-02-21 12:29:32 -0800

    update

--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -25,7 +25,7 @@ function Update() {
         axios.put(`http://localhost:7000/update/comp/${params.id}`,{
             "name":name,
             "title":title,
-           
+            "message":message
             }).then(()=>{
                 alert("success update data")
             }).catch(err=> console.log(err))
